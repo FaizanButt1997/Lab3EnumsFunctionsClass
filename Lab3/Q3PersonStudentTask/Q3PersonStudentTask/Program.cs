86804365770dfe4242784ceae32f40f826ec7c22
Faizan Butt <30572817+FaizanButt1997@users.noreply.github.com>
2018-08-02 08:00:39 -0700

    Q3

---
+++ Lab3/Q3PersonStudentTask/Q3PersonStudentTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3PersonStudentTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = false;
+
+            Person myPerson = new Person();
+            myPerson.Greet();
+
+            Student myStudent = new Student();
+            myStudent.SetAge(21);
+            myStudent.Greet();
+            myStudent.ShowAge();
+
+            Teacher myTeacher = new Teacher();
+            myTeacher.SetAge(30);
+            myTeacher.Greet();
+            myTeacher.Explain();
+
+          
+                Console.ReadLine();
+        }
+    }
+}
+
---
+++ Lab3/Q3PersonStudentTask/Q3PersonStudentTask/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3PersonStudentTask
+{
+   public class Teacher:Person
+    {
+        private string subject;
+
+        public void Explain()
+        {
+            Console.WriteLine("Explanation begins");
+        }
+
+    }
+}
